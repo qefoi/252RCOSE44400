54a621d5913388a7f84c74cef296ab5bda95f57f
qefoi <ehdbs0316@korea.ac.kr>
2025-11-25 17:04:57 +0900

    first commit

---
+++ front/app_front.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import requests
+
+app = Flask(__name__)
+
+# URL of the backend container inside Docker network
+BACKEND_URL = "http://backend:5001"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    resp = requests.get(BACKEND_URL+"/api/message")
+    data = resp.json()
+    message = data['message']
+    return render_template('index.html', current_message=message)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    """
+    TODO:
+    - Get the value from the form field named "new_message"
+    - Send a POST request to BACKEND_URL + "/api/message"
+      with JSON body { "message": new_message }
+    - Redirect back to "/"
+    """
+    pass
+
+
+# v2 TODO:
+# - Change page title (in HTML)
+# - Parse timestamp from backend message
+# - Show "Last updated at: <timestamp>" in the template
+
+
+if __name__ == "__main__":
+    # Do not change the host or port
+    app.run(host="0.0.0.0", port=5000)
